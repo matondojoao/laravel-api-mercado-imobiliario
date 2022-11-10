a8f182efb0f888b2a99adf28bd8236a41809c16a
Matondo João <97990105+matondojoao@users.noreply.github.com>
2022-11-10 18:09:13 +0100

    reparando Migrações das Tabelas

---
+++ database/migrations/2022_11_10_165915_create_table_user_profile.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableUserProfile extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profile', function (Blueprint $table) {
+            $table->id();
+            $table->text('about')->nullable(true);
+            $table->text('social_networks')->nullable(true);
+            $table->string('phone');
+            $table->string('mobile_phone');
+	        $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profile');
+    }
+}
