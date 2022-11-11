df1899a2a2ffdc563ed4ab86b1c40dd86660d1a5
Matondo João <97990105+matondojoao@users.noreply.github.com>
2022-11-11 18:12:27 +0100

    Create 2022_11_10_173130_create_table_real_state_categories.php

---
+++ database/migrations/2022_11_10_173130_create_table_real_state_categories.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableRealStateCategories extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('real_state_categories', function (Blueprint $table) {
+            $table->id();
+            $
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('real_state_categories');
+    }
+}
