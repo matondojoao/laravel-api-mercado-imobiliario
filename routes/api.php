92af32820864f31324c07ed66633a59daf274134
Matondo João <97990105+matondojoao@users.noreply.github.com>
2022-11-18 15:56:49 +0100

    Iniciando o endpoint de images

--- routes/api.php
+++ routes/api.php
@@ -5,22 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\RealStateController;
 use App\Http\Controllers\Api\V1\UserController;
 use App\Http\Controllers\Api\V1\CategoryController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::group(['prefix'=>'real-states'],function(){
    Route::get('/',[RealStateController::class,'index']);
    Route::post('/',[RealStateController::class,'store']);
    Route::get('/{id}',[RealStateController::class,'show']);
-   Route::post('/{id}',[RealStateController::class,'update']);
+   Route::put('/{id}',[RealStateController::class,'update']);
    Route::delete('/{id}',[RealStateController::class,'destroy']);
 });
 
