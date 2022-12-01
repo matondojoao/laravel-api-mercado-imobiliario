14b02699194284a29665980e77bcffc761cd927c
Matondo <matondojoaokitemoco@gmail.com>
2022-12-01 12:46:29 +0100

    Configurando User & Driver JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,10 @@ class User extends Authenticatable implements JWTSubject
     {
        return $this-getKey();
     }
+    public function getJWTCustomClaims()
+    {
+        return[
+
+        ];
+    }
 }
