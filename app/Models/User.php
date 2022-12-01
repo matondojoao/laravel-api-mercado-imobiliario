1631e85e26e3fbdf442a562ad640fcf09de7d646
Matondo <matondojoaokitemoco@gmail.com>
2022-12-01 12:38:52 +0100

    Configurando User & Driver JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use App\Models\RealState;
 use App\Models\UserProfile;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
