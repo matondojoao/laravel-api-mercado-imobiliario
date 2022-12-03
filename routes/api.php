575a4fb876ff26956261988d6f4f612a97c82faa
Matondo <matondojoaokitemoco@gmail.com>
2022-12-03 08:42:21 +0100

    Endpoint Login & Gerando Token

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,17 @@ use App\Http\Controllers\Api\V1\CategoryController;
 use App\Http\Controllers\Api\V1\RealStatePhotoController;
 use App\Http\Controllers\Api\V1\Auth\LoginJWTController;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
 Route::group(['prefix'=>'auth'],function(){
     Route::put('/login',[LoginJWTController::class,'login']);
 });
