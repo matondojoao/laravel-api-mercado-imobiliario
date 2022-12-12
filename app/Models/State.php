591237044e60f48fff6139324ca48bc1e91fa374
Matondo <matondojoaokitemoco@gmail.com>
2022-12-12 17:15:23 +0100

    Relação entre Models de Localidade

--- app/Models/State.php
+++ app/Models/State.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\City;
+use App\Models\Country;
 
 class State extends Model
 {
@@ -12,5 +13,9 @@ class State extends Model
 
     public function Cities(){
         return $this->hasMany(City::class);
+    }
+
+    public function Country(){
+        return $this->belongsTo(Country::class);
     }
 }
