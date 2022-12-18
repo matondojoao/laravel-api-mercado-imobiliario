0928e7d82cf1c375584471e7c538cac6cf7b64a5
Matondo <matondojoaokitemoco@gmail.com>
2022-12-18 10:49:25 +0100

    Busca de Imóveis

--- app/Http/Controllers/Api/V1/RealStateSearchController.php
+++ app/Http/Controllers/Api/V1/RealStateSearchController.php
@@ -9,8 +9,22 @@ use App\Models\RealState;
 class RealStateSearchController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
+       $RealState=new RealState();
+
+       if($request->has('conditions')){
+        $expressions=explode(';',$request->get('conditions'));
+
+       foreach($expressions as $e){
+          $exp=explode(':',$e);
+          $RealState=$RealState->where($exp[0],$exp[1],$exp[2]);
+         }
+       }
+       if($request->has('fields')){
+        $fields=$request->get('fields');
+        $RealState=$RealState->selectRaw($fields);
+       }
         $RealState=RealState::paginate(10);
         return response()->json($RealState, 200);
     }
